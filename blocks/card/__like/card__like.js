521039d1a208b70b12fb781a2cb53a17cbb0c082
slonpro <vlad.slonpro@gmail.com>
2021-03-14 13:55:35 +0300

    feat: У кнопки закрытия добавил интерактив

--- blocks/card/__like/card__like.js
+++ blocks/card/__like/card__like.js
@@ -1,5 +1,6 @@
 let likes = document.querySelectorAll('.card__like')
 
+//Закрашиваем сердечко на черное и если черное обратно
 for (let i = 0; i < likes.length; i++) {
   let like = likes[i];
   like.addEventListener('click', function likeget() {
